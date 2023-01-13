5234c49a7413b3222d3bba4b408da72f62bdbbbf
Samya brata Maji <samyabratamaji334@gmail.com>
2023-01-13 13:30:26 +0530

    [BUGFIX] loading static files

---
+++ sim.js
@@ -0,0 +1,16 @@
+let usp = new URLSearchParams(window.location.search);
+let usp_query = usp.get("run_sim");
+
+const runSimulation = () => {
+    document.getElementById('con-sim').style.display = 'block'
+    document.getElementById('root').style.display = 'none'
+}
+
+const abortSimulation = () => {
+    document.getElementById('con-sim').style.display = 'none'
+    document.getElementById('root').style.display = 'block'
+}
+
+if (usp_query === 'true') {
+    runSimulation()
+} else abortSimulation()
