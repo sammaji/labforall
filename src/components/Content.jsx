98b96adfb441ebb2fcb1e03ba13d6020d11bb914
Samya brata Maji <samyabratamaji334@gmail.com>
2023-01-14 19:48:21 +0530

    added data feching #34

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,78 +1,49 @@
 import React, { memo, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import "../assets/css/Content.css";
 import "../assets/css/Content.css";
 import { fetchExperiment } from "../utils/data/fetchExperiment.data";
-
-const DisplayExperiments = (props) => {
-  let observations = null;
-  if (typeof props.observations === "string") {
-    observations = <p>props.data.observations</p>;
-  } else if (Array.isArray(props.observations)) {
-    observations = (
-      <>
-        <ul>
-          {props.observations.map((item, index) => {
-            return <li key={index}>{item.content}</li>;
-          })}
-        </ul>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <h3>{props.aim || ""}</h3>
-      <p>{props.materials || ""}</p>
-    </>
-  );
-};
 
 const Content = () => {
   const { classId, subject, exp } = useParams();
   const [content, setContent] = useState(null);
 
-  let exp_data = <p>Loading...</p>
-
   useEffect(() => {
     fetchExperiment(classId, subject, exp).then((data) => {
-      console.log(data.aim);
       setContent(data);
-
-      let observations = undefined;
-      if (typeof props.observations === "string") {
-        observations = <p>props.observations</p>;
-      } else if (Array.isArray(props.observations)) {
-        observations = (
-          <>
-            <ul>
-              {props.observations.map((item, index) => {
-                return <li key={index}>{item.content}</li>;
-              })}
-            </ul>
-          </>
-        );
-      }
-
-      exp_data = (
-        <>
-          <h3>{props.aim || ""}</h3>
-          <p>{props.materials || "No materials required"}</p>
-        </>
-      );
+      console.log(data);
     });
   }, []);
 
   return (
-    <article>
+    <article className="exp-content">
       <div>
         <h1>Hello Sam</h1>
         <h2>Today's Experiments</h2>
       </div>
-
-      {/* <div>{exp_data}</div> */}
       <div>
-        <DisplayExperiments data={content}/>
+        <h3>{content?.Aim}</h3>
+        <p>{content?.MaterialsRequired || "No materials required"}</p>
+        <p>{content?.Theory || "No theory available"}</p>
+        <p className="new-line">
+          {content?.Procedure?.replaceAll("@", "\n").replaceAll(/(\\n)/g, "\n")}
+        </p>
+        <p className="new-line">
+          {content?.Obsevation?.replaceAll("@", "\n").replaceAll(
+            /(\\n)/g,
+            "\n"
+          ) || "Make necessary observations"}
+        </p>
+        {content?.Calculation?.replaceAll("@", "\n").replaceAll(
+          /(\\n)/g,
+          "\n"
+        ) || "No calculation required"}
+        <p className="new-line">
+          {content?.Precautions?.replaceAll("@", "\n").replaceAll(
+            /(\\n)/g,
+            "\n"
+          ) || "Follow labratory guidelines"}
+        </p>
       </div>
     </article>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { IoIosHelpCircleOutline } from "react-icons/io";
-
 import MENU_DATA from "../data/menu.data";
-
+import logo_with_text from "../assets/svg/logo-and-text.svg";
 import "../assets/css/nav_bar_style.css";
 import { Link } from "react-router-dom";
+
+const getImg = (type) => {
+  switch (type) {
+    case "earth":
+      return ic_earth;
+    case "graph":
+      return ic_graph;
+    case "chat":
+      return ic_chat;
+    case "book":
+      return ic_book;
+    default:
+      return ic_earth;
+  }
+};
 
 const NavBar = () => {
   // function toggle_menu() {
@@ -13,17 +26,12 @@ const NavBar = () => {
 
   return (
     <nav className="navbar hello">
-      {/* <header>
-        <button onClick={toggle_menu}>{"  <  "}</button>
-      </header> */}
+      <a href="https://lab4all.vercel.app"><img src={logo_with_text} /></a>
       <ul>
         {MENU_DATA.map((item, index) => {
           return (
             <Link to={item.to} key={index}>
-              <li>
-                <IoIosHelpCircleOutline />
-                {item.title}
-              </li>
+              <li>{item.title}</li>
             </Link>
           );
         })}
