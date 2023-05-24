8ab9f4f9930a889bc35c80df5d26ec4e96a74d33
Samya brata Maji <samyabratamaji334@gmail.com>
2023-05-24 21:00:46 +0530

    add: firebase emulators

--- src/utils/config/firebase.config.js
+++ src/utils/config/firebase.config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -15,5 +15,10 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+if (import.meta.env.VITE_USE_EMULATOR !== 'false' && import.meta.env.NODE_ENV !== 'production') {
+  connectAuthEmulator(db, "http://localhost:9099" )
+  connectFirestoreEmulator(db, "localhost", "8080")
+}
 
 export { auth, db };
